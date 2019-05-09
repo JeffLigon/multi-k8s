37bb99bffdb3433a6b599f9f10e4083b21600150
Jeff Ligon <jeff.ligon@live.com>
2019-05-09 14:12:15 -0500

    changed app name, instead of Welcome To React

--- client/src/App.js
+++ client/src/App.js
@@ -11,11 +11,10 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Fibonacci Sequence Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </header>
-        
+        </header>    
         <div>
           <Route exact path="/" component={Fib} />
           <Route exact path="/otherpage" component={OtherPage} />

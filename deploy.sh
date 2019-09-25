c9e0531f97a855f68454ee63c0797eb66fd3b6e2
Jeff Ligon <jeff.ligon@nokia.com>
2019-09-25 17:20:21 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 docker build -t jeffligon/multi-client:latest -t jeffligon/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t jeffligon/multi-server:latest -t jeffligon/multi-server:$SHA  -f ./server/Dockerfile ./server
 docker build -t jeffligon/multi-worker:latest -t jeffligon/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+
 docker push jeffligon/multi-client:latest
-docker push jeffligon/multi-client:$SHA
 docker push jeffligon/multi-server:latest
-docker push jeffligon/multi-server:$SHA
 docker push jeffligon/multi-worker:latest
+
+docker push jeffligon/multi-client:$SHA
+docker push jeffligon/multi-server:$SHA
 docker push jeffligon/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jeffligon/multi-server:$SHA
 kubectl set image deployments/client-deployment client=jeffligon/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=jeffligon/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=jeffligon/multi-worker:$SHA
   
